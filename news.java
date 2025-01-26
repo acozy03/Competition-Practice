90e0ac6e1e7755ca0b5e921c521fe63f517e1a5f
acozy03 <acozy03@gmail.com>
2025-01-26 15:42:39 -0500

    1/24 comp

--- news.java
+++ news.java
@@ -1,6 +1,45 @@
 import java.util.*;
+
 public class news {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        int numCases = scanner.nextInt();
+        
+        while (numCases > 0) {
+            int n = scanner.nextInt();
+            int[] supervisor = new int[n];
+        List<List<Integer>> tree = new ArrayList<>();
+    
+         for (int i = 0; i < n; i++) {
+                tree.add(new ArrayList<>());
+            }
+        
+            for (int i =1; i < n; i++) {
+                 supervisor[i] = scanner.nextInt();
+                tree.get(supervisor[i]).add(i);
+            }
+            int result = calculateTime(tree, 0);
+            System.out.println(result);
+            numCases--; 
+        }
+        
+        scanner.close();
+    }
+    private static int calculateTime(List<List<Integer>> tree, int node) {
+        List<Integer> times = new ArrayList<>();
+    
+        for (int child: tree.get(node)) {
+            times.add(calculateTime(tree, child));
+        }
+        Collections.sort(times, Collections.reverseOrder());
+        
+        int maxTime = 0;
+  
+        for (int i = 0; i < times.size(); i++) {
+            int addtoItime = times.get(i) + i + 1;  
+            maxTime = Math.max(maxTime, addtoItime);
+        }
+        return maxTime;
     }
 }
--- sorting.java
+++ sorting.java
@@ -1,6 +1,30 @@
 import java.util.*;
 public class sorting {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int numCases = Integer.parseInt(scanner.nextLine()); 
+
+        for (int i = 0; i < numCases; i++) {
+            // take in # on next line 
+            int numPapers = Integer.parseInt(scanner.nextLine());
+            PriorityQueue<Integer> papers = new PriorityQueue<>(); 
+            for (int j = 0; j < numPapers; j++) {
+                papers.add(scanner.nextInt());
+            }
+            scanner.nextLine(); 
+            System.out.println(stackingPapers(papers, numPapers));
+        }
+        scanner.close();
+    }
+
+    public static int stackingPapers(PriorityQueue<Integer> papers, int numPapers) {
+        if (numPapers == 1)
+            return 0; 
+        int a = papers.poll(); 
+        int b = papers.poll();
+        int res = a+b;
+        papers.add(res); 
+        numPapers--; 
+        return res + stackingPapers(papers, numPapers);
     }
 }
