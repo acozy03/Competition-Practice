d556f2461bce43773fe86c2f486bc70cf15c886b
acozy03 <acozy03@gmail.com>
2025-02-14 10:47:52 -0500

    2/14 comp

--- ant.java
+++ ant.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class ant {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in); 
+        int n = scanner.nextInt(); 
+        for (int i = 0; i < n; i++) {
+            int numHills, numTunnels; 
+            numHills = scanner.nextInt();
+            numTunnels = scanner.nextInt();
+            scanner.nextLine();
+            for(int j = 0; j < numTunnels; j++) {
+                int x,y,cost; 
+                x = scanner.nextInt(); 
+                y = scanner.nextInt();
+                cost = scanner.nextInt();
+                
+            }
+        }
+        scanner.close();
     }  
+}
+
+class graph {
+
 }
