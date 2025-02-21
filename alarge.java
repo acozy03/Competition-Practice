f69b93da23567d28d115a9c7093ba7ff1fc0ff2e
acozy03 <acozy03@gmail.com>
2025-02-21 08:53:49 -0500

    more 1/14

--- alarge.java
+++ alarge.java
@@ -1,6 +1,91 @@
 import java.util.*;
+
 public class alarge {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numCases = scanner.nextInt(); 
+
+        while (numCases > 0) {
+            int numLocations = scanner.nextInt(); 
+            int numRoads = scanner.nextInt(); 
+            int maxCrosswalks = scanner.nextInt(); 
+
+            List<List<Edge>> graph = new ArrayList<>();
+            for (int i = 0; i <= numLocations; i++) {
+                graph.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < numRoads; i++) {
+                int startRoad = scanner.nextInt();
+                int endRoad = scanner.nextInt();
+                int isCrosswalk = scanner.nextInt();
+                int lengthOfRoad = scanner.nextInt(); // length
+                graph.get(startRoad).add(new Edge(endRoad, isCrosswalk, lengthOfRoad));
+                graph.get(endRoad).add(new Edge(startRoad, isCrosswalk, lengthOfRoad)); // has to go both directions
+            }
+
+            int res = dijkstra(graph, numLocations, maxCrosswalks);
+
+            System.out.println(res);
+            numCases--; 
+        }
+        scanner.close();
+    }
+
+    private static int dijkstra(List<List<Edge>> graph, int numLocations, int maxCrosswalks) {
+        // dist[v][k] = shortest length to vertex v using k crosswalks
+        int[][] dist = new int[numLocations + 1][maxCrosswalks + 1];
+        for (int[] row : dist) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        //enqueue ordered pairs of vertex number and number of crosswalks
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{0, 1, 0}); // vertex 1 with 0 crosswalks and 0 length
+        dist[1][0] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int currDist = current[0];
+            int currVertex = current[1];
+            int currCrosswalks = current[2];
+
+            //Stop looking for paths once ANY path to the class is found
+            if (currVertex == numLocations) {
+                return currDist;
+            }
+
         
+            if (currDist > dist[currVertex][currCrosswalks]) {
+                continue;
+            }
+
+            for (Edge edge : graph.get(currVertex)) {
+                int nextVertex, nextCrosswalks, nextDist;
+                 nextVertex = edge.destinationVertex;
+                nextCrosswalks = currCrosswalks + edge.isCrosswalk;
+                nextDist = currDist + edge.length;
+
+                // consider paths with only <= maxCrosswalks crosswalks
+                if (nextCrosswalks <= maxCrosswalks && nextDist < dist[nextVertex][nextCrosswalks]) {
+                    dist[nextVertex][nextCrosswalks] = nextDist;
+                    pq.offer(new int[]{nextDist, nextVertex, nextCrosswalks});
+                }
+            }
+        }
+        // could not find path
+        return -1;
+    }
+
+    static class Edge {
+        int destinationVertex; 
+        int isCrosswalk; 
+        int length; 
+
+        Edge(int destinationVertex, int isCrosswalk, int length) {
+            this.destinationVertex = destinationVertex;
+            this.isCrosswalk = isCrosswalk;
+            this.length = length;
+        }
     }
 }
--- ant.java
+++ ant.java
@@ -3,117 +3,91 @@ import java.util.*;
 public class ant {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt(); // Number of campuses
-        
-        for (int i = 0; i < n; i++) {
-            int numHills = scanner.nextInt();
-            int numTunnels = scanner.nextInt();
-            Graph graph = new Graph(numHills);
+        int numCampuses = scanner.nextInt(); 
 
-            for (int j = 0; j < numTunnels; j++) {
+        for (int campus = 1; campus <= numCampuses; campus++) {
+            int numHills = scanner.nextInt();
+            int numTunnels = scanner.nextInt(); 
+            List<Edge> edges = new ArrayList<>();
+
+            for (int i = 0; i < numTunnels; i++) {
                 int x = scanner.nextInt();
                 int y = scanner.nextInt();
                 int cost = scanner.nextInt();
-                graph.addEdge(x, y, cost);
+                edges.add(new Edge(x, y, cost));
+            }
+            Collections.sort(edges, Comparator.comparingInt(e -> e.cost));
+            UnionFind unionFind = new UnionFind(numHills + 1); // +1 because ant hills are numbered from 1 to numHills
+
+            // Kruskal's
+            int totalCost = 0;
+            int edgesUsed = 0;
+
+            for (Edge edge : edges) {
+                if (unionFind.union(edge.x, edge.y)) { // if union is successful no cycle
+                    totalCost += edge.cost;
+                    edgesUsed++;
+                    if (edgesUsed == numHills - 1) break; 
+                }
             }
 
-            int result = graph.kruskalMST();
-            if (result == -1) {
-                System.out.println("Campus #" + (i + 1) + ": I'm a programmer, not a miracle worker!");
+            // check if ALL ant hills connected
+            if (edgesUsed == numHills - 1) {
+                System.out.println("Campus #" + campus + ": " + totalCost);
             } else {
-                System.out.println("Campus #" + (i + 1) + ": " + result);
+                System.out.println("Campus #" + campus + ": I'm a programmer, not a miracle worker!");
             }
         }
-
         scanner.close();
     }
-}
 
-// Edge class to store tunnels
-class Edge implements Comparable<Edge> {
-    int x, y, cost;
+    static class Edge {
+        int x, y, cost;
 
-    public Edge(int x, int y, int cost) {
-        this.x = x;
-        this.y = y;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge other) {
-        return Integer.compare(this.cost, other.cost); // Sort edges by cost (ascending)
-    }
-}
-
-// Disjoint Set (Union-Find) for Kruskal's Algorithm
-class DisjointSet {
-    int[] parent, rank;
-
-    public DisjointSet(int size) {
-        parent = new int[size + 1];
-        rank = new int[size + 1];
-        for (int i = 1; i <= size; i++) {
-            parent[i] = i;
-            rank[i] = 1;
+        Edge(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
         }
     }
 
-    public int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // Path compression
-        }
-        return parent[x];
-    }
+    static class UnionFind {
+        int[] parent;
+        int[] rank;
 
-    public boolean union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        
-        if (rootX == rootY) return false; // Already connected
-        
-        // Union by rank
-        if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else {
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
-        return true;
-    }
-}
-
-// Graph class
-class Graph {
-    private int numHills;
-    private List<Edge> edges;
-
-    public Graph(int numHills) {
-        this.numHills = numHills;
-        this.edges = new ArrayList<>();
-    }
-
-    public void addEdge(int x, int y, int cost) {
-        edges.add(new Edge(x, y, cost));
-    }
-
-    public int kruskalMST() {
-        if (numHills == 1) return 0; // If only one hill, no cost needed
-
-        Collections.sort(edges); // Sort edges by weight
-        DisjointSet ds = new DisjointSet(numHills);
-
-        int totalCost = 0, edgesUsed = 0;
-        for (Edge edge : edges) {
-            if (ds.union(edge.x, edge.y)) {
-                totalCost += edge.cost;
-                edgesUsed++;
-                if (edgesUsed == numHills - 1) break; // MST is complete
+        UnionFind(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                rank[i] = 1;   
             }
         }
 
-        // If we haven't used (h - 1) edges, the graph isn't fully connected
-        return (edgesUsed == numHills - 1) ? totalCost : -1;
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // path compression
+            }
+            return parent[x];
+        }
+
+        boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) return false; // already in the same set
+
+            // union by rank
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+
+            return true; 
+        }
     }
 }
